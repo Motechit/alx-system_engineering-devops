145718fa909639e780e23f8eb00f110e50eb9b38
Motechit <otikomosunmola@gmail.com>
2024-01-16 18:21:49 -0800

    api-advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""This script queries  the Reddit API and returns
-the number of subscribers"""
-
+""" This function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """This fnx returns the num of subscribers 
-    for a given subreddit"""
-
-    apiUrl = "https://reddit.com/r/{}/about.json".format(subreddit)
-    userAgent = "Mozilla/5.0"
-
-    response = requests.get(apiUrl, headers={"user-agent": userAgent})
-    if not response:
+    """It return the total num of subscribers on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    retValue = response.json().get('data').get('subscribers')
-    if retValue:
-        return retValue
-    else:
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
