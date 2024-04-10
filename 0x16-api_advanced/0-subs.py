6b41d13948b97fac916c4152a058b333b24c4039
motechit <otikomosunmola@gmail.com>
2024-04-10 18:55:05 +0000

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,14 +11,12 @@ def number_of_subscribers(subreddit):
     and returns the number of subscribers (not active 
     users, total subscribers) for a given subreddit"""
     
-    apiUrl = "https://reddit.com/r/{}/about.json".format(subreddit)
-    userAgent = "Mozilla/5.0"
-
-    response = requests.get(apiUrl, headers={"user-agent": userAgent})
-    if not response:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "linux:0x16.api.advanced:v1.0.0 (by /u/Relevant-Repair-6162)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    retValue = response.json().get('data').get('subscribers')
-    if retValue:
-        return retValue
-    else:
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
